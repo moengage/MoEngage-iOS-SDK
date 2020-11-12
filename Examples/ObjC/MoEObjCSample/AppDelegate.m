a11f45e96def70a24c143240357d23d87a974382
Chengappa C D <chengappa@moengage.com>
2020-11-12 16:35:05 +0530

    Example Project changes

--- Examples/ObjC/MoEObjCSample/AppDelegate.m
+++ Examples/ObjC/MoEObjCSample/AppDelegate.m
@@ -27,7 +27,7 @@
     
     // MoEngage SDK Initialization
     //TODO: Add your MoEngage App ID
-    NSString* yourMoEngageAppID = @"DAO6UGZ73D9RTK8B5W96TPYN";
+    NSString* yourMoEngageAppID = @"Your App ID";
     #ifdef DEBUG
         [[MoEngage sharedInstance] initializeDevWithAppID:yourMoEngageAppID  withLaunchOptions:launchOptions];
     #else
--- Examples/Swift/MoESwiftSample/AppDelegate.swift
+++ Examples/Swift/MoESwiftSample/AppDelegate.swift
@@ -26,7 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         //MoEngage.debug(LOG_ALL)
         
         //TODO: Add your MoEngage App ID
-        var yourMoEAppID = "DAO6UGZ73D9RTK8B5W96TPYN"
+        var yourMoEAppID = "Your App ID"
         MoEngage.debug(LOG_ALL)
         DispatchQueue.main.async {
             #if DEBUG
