a44763a7161c321da6eca31836e4e470f7d3340c
msoumya-engg-sdk <soumya.mahunt@moengage.com>
2024-12-17 14:01:20 +0530

    MOEN-35365: Added CocoaPods publish action

---
+++ Utilities/pod_publish.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby
+
+# Usage:
+# Publish pods to CocoaPods
+
+require 'fileutils'
+require 'json'
+require 'ostruct'
+
+config = JSON.parse(File.read('package.json'), {object_class: OpenStruct})
+derived_data = File.expand_path('~/Library/Developer/Xcode/DerivedData')
+config.packages.each do |package|
+  podspec = "#{package.name}.podspec"
+  next unless File.exist?(podspec)
+  begin
+    FileUtils.rm_rf(derived_data) if File.directory?(derived_data)
+    system("pod trunk push \"#{podspec}\" --verbose --synchronous --allow-warnings", out: STDOUT, exception: true)
+    sleep 1 # even with --synchronous flag push might fail for depending pods
+  rescue => error
+    puts "::warning::Pod published failed with #{error&.message}"
+  end
+end
